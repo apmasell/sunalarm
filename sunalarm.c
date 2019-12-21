f7a9b516e2f03f007f880a625436ce568424d942
Andre Masella <andre@masella.name>
2019-12-20 21:47:40 -0500

    Make changes to reduce interrupt jitter
    * set the TCCR1A register to zero too
    * remove LED logic from zero crossing ISR
    * replace all double calculations with integer ones
    * cache the tick calculation
    * disable timer 0
    * change prescaler frequency
    * measure AC zero cross interval multiple times

--- sunalarm.c
+++ sunalarm.c
@@ -1,55 +1,93 @@
+#define F_CPU 16000000UL
+
 #include<avr/interrupt.h>
 #include<avr/io.h>
 #include<avr/sleep.h>
+#include<util/delay.h>
 #include<stdbool.h>
+#include<stdlib.h>
 
 #define WAKEUP 6
 #define DAWN_INTERVAL 110
 
-int ticksPerHalfWave = 0;
-int cycleCounter = 0;
-int minuteCounter = (21 - WAKEUP) * 60 - DAWN_INTERVAL;	// 9PM
+// Whether we have completed calibration or not
+volatile bool initialised = false;
+// The number of timer ticks we sould delay for each minute in our simulated dawn. This is the amount of AC leading wave that we are chopping off. OR the total number of ticks per AC cycle we have seen (during calibration).
+int dawnTicksPerHalfWave[DAWN_INTERVAL];
+// The current number of AC cycles we have seen this minute (normally); or the total number of AC cycles we have seen (during calibraiton).
+volatile int cycleCounter = 0;
+// The number of minutes per day; initialised to be when you plug the thing in
+volatile int minuteCounter = (22 - WAKEUP) * 60 + DAWN_INTERVAL;	// 10PM
+
+int cmpfunc(const void *a, const void *b)
+{
+	return (*(int *)a - *(int *)b);
+}
 
 ISR(INT0_vect)
 {
-	if (ticksPerHalfWave == 0) {
-		if (TCNT1 > 0) {
-			ticksPerHalfWave = TCNT1;
+	if (!initialised) {
+		// Determining the correct time count is hard, so we're going to do it
+		// experimentally (as a side benefit, it will autocalibrate between 50/60Hz
+		// power sources); we measure the delay between AC zero crossings many times.
+
+		// Stop the timer
+		TCCR1A = 0;
+		TCCR1B = 0;
+		const int currentCount = TCNT1;
+		// If we got a time value, store it in our array
+		if (currentCount > 0) {
+			dawnTicksPerHalfWave[cycleCounter++] = currentCount;
+		}
+		// If we've seen enough cycles, do our calibration
+		if (cycleCounter == DAWN_INTERVAL) {
+			// Reset for the normal operation
+			cycleCounter = 0;
+			// Sort our samples and take the median
+			qsort(dawnTicksPerHalfWave, DAWN_INTERVAL,
+			      sizeof(dawnTicksPerHalfWave[0]), &cmpfunc);
+			const int ticksPerHalfWave =
+			    dawnTicksPerHalfWave[DAWN_INTERVAL / 2];
+			// Compute the delay for each of our dawn minutes
+			for (int i = 0; i < DAWN_INTERVAL; i++) {
+				const int tickDelay = (DAWN_INTERVAL -
+						       i) *
+				    ticksPerHalfWave / DAWN_INTERVAL;
+				dawnTicksPerHalfWave[i] =
+				    tickDelay < 1 ? 1 : tickDelay;
+			}
+			initialised = true;
+			// Turn on the built-in LED
 			PORTD |= (1 << PD6);
 		} else {
-			TCCR1B = (1 << CS12) | (1 << CS10);
-			return;
+			// If we haven't seen enough AC cycles, reset the timer and capture another
+			TCNT1 = 0;
+			TCCR1A = 0;
+			TCCR1B = (1 << CS12);
 		}
+		return;
 	}
-	const bool requestedOn = !(PINB & (1 << PB1));
 	cycleCounter = (cycleCounter + 1) % 7200;	// AC half-cycles per minute
-	if (requestedOn) {
-		if (cycleCounter % 60 == 0) {
-			PORTD ^= (1 << PD6);
-		}
-	} else {
-		if (cycleCounter % 120 == 80) {
-			PORTD |= (1 << PD6);
-		} else if (cycleCounter % 120 == 0
-			   && cycleCounter / 120 <=
-			   (minuteCounter / 60 + WAKEUP) % 24) {
-			PORTD &= ~(1 << PD6);
-		}
-	}
 	if (cycleCounter == 0) {
 		minuteCounter = (minuteCounter + 1) % 1440;	// minutes per day
 	}
+	const bool requestedOn = !(PINB & (1 << PB1));
 	if (requestedOn) {	// Requested on
 		PORTD |= (1 << PD2);
 	} else if (minuteCounter < DAWN_INTERVAL) {	// Dawn
+		// Make sure the AC power is off
 		PORTD &= ~(1 << PD2);
+		// Rest the timer
+		TCCR1A = 0;
 		TCCR1B = 0;
-		const int tickDelay = (1.0 -
-				       ((double)minuteCounter) /
-				       DAWN_INTERVAL) * ticksPerHalfWave;
-		OCR1A = tickDelay < 1 ? 1 : tickDelay;
 		TCNT1 = 0;
-		TCCR1B = (1 << CS12) | (1 << CS10);
+		// The timer is going to fire two interrupts A to turn on the AC, and B to turn off the signal to the AC (since the TRIAC will stay on)
+		TIFR1 |= (1 << OCF1A) | (1 << OCF1B);
+		OCR1A = dawnTicksPerHalfWave[minuteCounter];
+		OCR1B = dawnTicksPerHalfWave[minuteCounter] + 10;
+		// Start the timer
+		TCCR1A = 0;
+		TCCR1B = (1 << CS12);
 	} else if (minuteCounter < 2 * DAWN_INTERVAL) {	// Morning period always on
 		PORTD |= (1 << PD2);
 	} else {		// Always off
@@ -57,20 +95,39 @@ ISR(INT0_vect)
 	}
 }
 
+/*
+ * When we have delayed long enough, turn on the AC (i.e., trigger the TRIAC).
+ */
 ISR(TIMER1_COMPA_vect)
 {
-	PORTD |= (1 << PD2);
-	TCCR1B = 0;
+	if (initialised) {
+		PORTD |= (1 << PD2);
+	}
+}
+
+/*
+ * The TRIAC will remain on until the zero cross, but we're going to turn the pulse off, so we don't accidentally get to the next cycle and introduce flicker
+ */
+ISR(TIMER1_COMPB_vect)
+{
+	if (initialised) {
+		TCCR1A = 0;
+		TCCR1B = 0;
+		PORTD &= ~(1 << PD2);
+	}
 }
 
 int main()
 {
 	// Set pin 1 (AC driver) and 6 (built-in LED) as output
 	DDRD = (1 << PD2) | (1 << PD6);
+	// Disable timer 0
+	TCCR0B = 0;
+	TCCR0A = 0;
 	// Each AC half-cycle is about 8.333ms
 	// Run clock at 256th speed; there are 520 ticks per AC half-cycle
 	// Enable interrupt
-	TIMSK1 = (1 << OCIE1A);
+	TIMSK1 = (1 << OCIE1A) | (1 << OCIE1B);
 
 	// Rising edge triggered
 	EICRA = (1 << ISC01) | (1 << ISC00);
@@ -79,5 +136,25 @@ int main()
 	TCNT1 = 0;
 
 	sei();
-	while (true) ;
+	while (!initialised) ;
+	while (true) {
+		const bool requestedOn = !(PINB & (1 << PB1));
+		if (requestedOn) {
+			if (cycleCounter % 60 == 0) {
+				if ((cycleCounter / 60) % 2 == 0) {
+					PORTD &= ~(1 << PD6);
+				} else {
+					PORTD &= ~(1 << PD6);
+				}
+			}
+		} else {
+			if (cycleCounter % 120 == 80) {
+				PORTD |= (1 << PD6);
+			} else if (cycleCounter % 120 == 0
+				   && cycleCounter / 120 <=
+				   (minuteCounter / 60 + WAKEUP) % 12) {
+				PORTD &= ~(1 << PD6);
+			}
+		}
+	}
 }
